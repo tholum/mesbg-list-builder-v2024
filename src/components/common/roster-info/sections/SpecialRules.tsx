56bffb7034b4230a016874440d710fbdbb6cbd2c
mhollink <hollink.mh@gmail.com>
2024-12-28 11:44:16 +0100

    disable troll upgrades in game mode

--- src/components/common/roster-info/sections/SpecialRules.tsx
+++ src/components/common/roster-info/sections/SpecialRules.tsx
@@ -17,10 +17,13 @@ import { RosterInformationProps } from "../RosterInformation.tsx";
 import { RosterInformationSection } from "../RosterInformationSection.tsx";
 
 export const SpecialRules: FunctionComponent<
-  Pick<RosterInformationProps, "roster"> & { size?: "dense" | "normal" }
-> = ({ roster, size = "normal" }) => {
+  Pick<RosterInformationProps, "roster" | "editable"> & {
+    size?: "dense" | "normal";
+  }
+> = ({ roster, editable, size = "normal" }) => {
   const armyListMetadata = (armyListData as ArmyListData)[roster.armyList];
   const { updateRoster } = useRosterBuildingState();
+
   const trollUpgrades = roster.metadata.tttSpecialUpgrades || [];
 
   if (!armyListMetadata || armyListMetadata.special_rules.length === 0)
@@ -82,51 +85,58 @@ export const SpecialRules: FunctionComponent<
   return (
     <RosterInformationSection title="Special rules">
       <Box component="ul" sx={{ listStyle: "none", pl: 2 }}>
-        {armyListMetadata.special_rules.map((rule, index) => (
-          <Box
-            component="li"
-            key={index}
-            sx={{ py: size === "normal" ? 1 : 0 }}
-          >
-            {isMovieQuote(rule.title) ? (
-              <Typography sx={{ ml: rule.troll_purchase === true ? -1 : 0 }}>
-                {rule.troll_purchase === true && (
-                  <CustomSwitch
-                    checked={trollUpgrades.includes(rule.title)}
-                    onChange={(_, checked) =>
-                      changeRuleState(rule.title, checked)
-                    }
-                  />
-                )}
-                <b>
-                  <i>{rule.title}</i>
-                </b>
-              </Typography>
-            ) : (
-              <Typography sx={{ ml: rule.troll_purchase === true ? -1 : 0 }}>
-                {rule.troll_purchase === true && (
-                  <CustomSwitch
-                    checked={trollUpgrades.includes(rule.title)}
-                    onChange={(_, checked) =>
-                      changeRuleState(rule.title, checked)
-                    }
-                  />
-                )}
-                <b>
-                  {rule.title}{" "}
-                  {rule.title === "A Troll's Hoard" && (
-                    <i>({trollUpgrades.length * 50} points)</i>
+        {armyListMetadata.special_rules
+          .filter((rule) => {
+            if (!rule.troll_purchase) return true;
+            return editable || trollUpgrades.includes(rule.title);
+          })
+          .map((rule, index) => (
+            <Box
+              component="li"
+              key={index}
+              sx={{ py: size === "normal" ? 1 : 0 }}
+            >
+              {isMovieQuote(rule.title) ? (
+                <Typography sx={{ ml: rule.troll_purchase === true ? -1 : 0 }}>
+                  {rule.troll_purchase === true && (
+                    <CustomSwitch
+                      checked={trollUpgrades.includes(rule.title)}
+                      onChange={(_, checked) =>
+                        changeRuleState(rule.title, checked)
+                      }
+                      disabled={!editable}
+                    />
                   )}
-                </b>
-              </Typography>
-            )}
-            <Stack gap={1}>
-              {rule.description.split("\n").map((line, index) => (
-                <Typography key={index}>{line}</Typography>
-              ))}
-            </Stack>
-          </Box>
-        ))}
+                  <b>
+                    <i>{rule.title}</i>
+                  </b>
+                </Typography>
+              ) : (
+                <Typography sx={{ ml: rule.troll_purchase === true ? -1 : 0 }}>
+                  {rule.troll_purchase === true && (
+                    <CustomSwitch
+                      checked={trollUpgrades.includes(rule.title)}
+                      onChange={(_, checked) =>
+                        changeRuleState(rule.title, checked)
+                      }
+                      disabled={!editable}
+                    />
+                  )}
+                  <b>
+                    {rule.title}{" "}
+                    {rule.title === "A Troll's Hoard" && (
+                      <i>({trollUpgrades.length * 50} points)</i>
+                    )}
+                  </b>
+                </Typography>
+              )}
+              <Stack gap={1}>
+                {rule.description.split("\n").map((line, index) => (
+                  <Typography key={index}>{line}</Typography>
+                ))}
+              </Stack>
+            </Box>
+          ))}
       </Box>
     </RosterInformationSection>
   );
