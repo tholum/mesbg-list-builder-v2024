f45b69a32074226b2a5e4466372d0be0fbba98e7
mhollink <hollink.mh@gmail.com>
2025-01-02 15:36:57 +0100

    Added save image button

--- src/components/modal/modals/RosterSummaryScreenshotModal.tsx
+++ src/components/modal/modals/RosterSummaryScreenshotModal.tsx
@@ -1,21 +1,34 @@
-import { DialogContent } from "@mui/material";
+import { Button, DialogContent } from "@mui/material";
+import { useRosterInformation } from "../../../hooks/useRosterInformation.ts";
 import { useAppState } from "../../../state/app";
 
 export const RosterSummaryScreenshotModal = () => {
   const { modalContext } = useAppState();
+  const { roster } = useRosterInformation();
   return (
     <>
       <DialogContent>
         {modalContext.screenshot != null && (
-          <img
-            src={modalContext.screenshot}
-            alt="roster screenshot"
-            style={{
-              margin: "1rem 0",
-              border: "1px solid black",
-              boxShadow: "1px 1px 5px 0px #000000AA",
-            }}
-          />
+          <>
+            <img
+              src={modalContext.screenshot}
+              alt="roster screenshot"
+              style={{
+                margin: "1rem 0",
+                border: "1px solid black",
+                boxShadow: "1px 1px 5px 0px #000000AA",
+                width: "100%",
+              }}
+            />
+            <Button
+              fullWidth
+              variant="contained"
+              href={modalContext.screenshot}
+              download={roster.id + ".png"}
+            >
+              Save image
+            </Button>
+          </>
         )}
       </DialogContent>
     </>
