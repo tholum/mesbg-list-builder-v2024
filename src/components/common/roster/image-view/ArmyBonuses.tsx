631ff4463b89274cb1caddbf3d6b3e2f6dc7fd7b
mhollink <hollink.mh@gmail.com>
2025-01-02 12:39:01 +0100

    wrap additional rules and special rules to hide headers for empty sections

--- src/components/common/roster/image-view/ArmyBonuses.tsx
+++ src/components/common/roster/image-view/ArmyBonuses.tsx
@@ -13,61 +13,69 @@ export const ArmyBonuses = () => {
 
   return (
     <Stack>
-      <Typography
-        variant="h6"
-        color="#800000"
-        fontWeight="bold"
-        textAlign="center"
-      >
-        Additional Rules
-      </Typography>
-      {armyListMetadata?.additional_rules?.map((rule, index) => (
-        <Typography key={index}>
-          &#9679;&nbsp;&nbsp;{rule.description}
-        </Typography>
-      ))}
-
-      <Typography
-        variant="h6"
-        color="#800000"
-        fontWeight="bold"
-        textAlign="center"
-        sx={{ mt: 2 }}
-      >
-        Special Rules
-      </Typography>
-      <Box component="ul" sx={{ listStyle: "none", pl: 0, mb: 1 }}>
-        {armyListMetadata.special_rules
-          .filter((rule) => {
-            if (!rule.troll_purchase) return true;
-            return tttSpecialUpgrades.includes(rule.title);
-          })
-          .map((rule, index) => (
-            <Box component="li" key={index} sx={{ py: 1 }}>
-              {isMovieQuote(rule.title) ? (
-                <Typography>
-                  <b>
-                    <i>{rule.title}</i>
-                  </b>
-                </Typography>
-              ) : (
-                <Typography>
-                  <b>
-                    {rule.title}{" "}
-                    {rule.title === "A Troll's Hoard" && (
-                      <i>({tttSpecialUpgrades.length * 50} points)</i>
-                    )}
-                  </b>
-                </Typography>
-              )}
-              <Stack gap={1}>
-                {rule.description.split("\n").map((line, index) => (
-                  <Typography key={index}>{line}</Typography>
-                ))}
-              </Stack>
-            </Box>
+      {armyListMetadata?.additional_rules?.length > 0 && (
+        <>
+          <Typography
+            variant="h6"
+            color="#800000"
+            fontWeight="bold"
+            textAlign="center"
+          >
+            Additional Rules
+          </Typography>
+          {armyListMetadata.additional_rules.map((rule, index) => (
+            <Typography key={index}>
+              &#9679;&nbsp;&nbsp;{rule.description}
+            </Typography>
           ))}
-      </Box>
+        </>
+      )}
+
+      {armyListMetadata?.special_rules?.length > 0 && (
+        <>
+          <Typography
+            variant="h6"
+            color="#800000"
+            fontWeight="bold"
+            textAlign="center"
+            sx={{ mt: 2 }}
+          >
+            Special Rules
+          </Typography>
+          <Box component="ul" sx={{ listStyle: "none", pl: 0, mb: 1 }}>
+            {armyListMetadata.special_rules
+              .filter((rule) => {
+                if (!rule.troll_purchase) return true;
+                return tttSpecialUpgrades.includes(rule.title);
+              })
+              .map((rule, index) => (
+                <Box component="li" key={index} sx={{ py: 1 }}>
+                  {isMovieQuote(rule.title) ? (
+                    <Typography>
+                      <b>
+                        <i>{rule.title}</i>
+                      </b>
+                    </Typography>
+                  ) : (
+                    <Typography>
+                      <b>
+                        {rule.title}{" "}
+                        {rule.title === "A Troll's Hoard" && (
+                          <i>({tttSpecialUpgrades.length * 50} points)</i>
+                        )}
+                      </b>
+                    </Typography>
+                  )}
+                  <Stack gap={1}>
+                    {rule.description.split("\n").map((line, index) => (
+                      <Typography key={index}>{line}</Typography>
+                    ))}
+                  </Stack>
+                </Box>
+              ))}
+          </Box>
+        </>
+      )}
       <Divider sx={{ mb: 2, height: 2, bgcolor: "#800000" }} />
     </Stack>
   );
