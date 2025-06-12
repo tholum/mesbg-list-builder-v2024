46f422694532f69e5b73997b82ee032a076360f2
Alex Cordaro <alex_cordaro@hotmail.co.uk>
2025-06-12 21:51:53 +0100

    Good legacy lists finished

--- data/scripts/mappers/army_list_data_converter.py
+++ data/scripts/mappers/army_list_data_converter.py
@@ -9,8 +9,9 @@ class ArmyListDataConverter:
         self.__army_lists['special_rules'] = self.__army_lists['special_rules'].apply(eval)
         self.__army_lists['rule_highlights'] = self.__army_lists['rule_highlights'].apply(eval)
         self.__army_lists['break_point'] = self.__army_lists['break_point'].fillna("")
+        self.__army_lists['legacy'] = self.__army_lists['legacy'].apply(lambda x: True if x == 1 else "")
 
-        self.__army_lists = self.__army_lists[['additional_rules', 'special_rules', 'bow_limit', 'throw_limit', 'break_point', 'rule_highlights']]
+        self.__army_lists = self.__army_lists[['additional_rules', 'special_rules', 'bow_limit', 'throw_limit', 'break_point', 'rule_highlights', 'legacy']]
         json_dict = self.__army_lists.to_dict(orient='index')
         json_dict = {f:{k:v for k,v in json_dict[f].items() if v != ""} for f in json_dict.keys()}
 
