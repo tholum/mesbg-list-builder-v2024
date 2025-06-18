9ca2ffaf6ec33e6b7321e619b374606a734d9a6b
mhollink <hollink.mh@gmail.com>
2025-06-18 22:24:43 +0200

    Added utility to automatically create the warband full of woses in Paths of the Druadan.

--- src/components/modal/modals/CreateNewRosterModal.tsx
+++ src/components/modal/modals/CreateNewRosterModal.tsx
@@ -20,13 +20,17 @@ import { v4 as randomUuid } from "uuid";
 import data from "../../../assets/data/mesbg_data.json";
 import models from "../../../assets/data/mesbg_data.json";
 import warningRules from "../../../assets/data/warning_rules.json";
+import { mesbgData } from "../../../assets/data.ts";
 import { useCalculator } from "../../../hooks/useCalculator.ts";
 import { useExport } from "../../../hooks/useExport.ts";
 import { useAppState } from "../../../state/app";
 import { useRosterBuildingState } from "../../../state/roster-building";
-import { emptyRoster } from "../../../state/roster-building/roster";
+import {
+  emptyRoster,
+  emptyWarband,
+} from "../../../state/roster-building/roster";
 import { ArmyType } from "../../../types/mesbg-data.types.ts";
-import { Roster } from "../../../types/roster.ts";
+import { Roster, Warband } from "../../../types/roster.ts";
 import { WarningRules } from "../../../types/warning-rules.types.ts";
 import { slugify, withSuffix } from "../../../utils/string.ts";
 import { CustomAlert } from "../../common/alert/CustomAlert.tsx";
@@ -92,6 +96,46 @@ export const CreateNewRosterModal = () => {
       jsonImportTextField.current.scrollTop =
         jsonImportTextField.current.scrollHeight;
     }
+  }
+
+  function wosesWarband(): Warband {
+    const ghan = mesbgData["[paths-of-the-druadan] ghan-buri-ghan"];
+    const woses = mesbgData["[paths-of-the-druadan] woses-warrior"];
+
+    return calculator.recalculateWarband({
+      id: randomUuid(),
+      hero: calculator.recalculatePointsForUnit({
+        ...ghan,
+        id: randomUuid(),
+        pointsPerUnit: ghan.base_points,
+        pointsTotal: ghan.base_points,
+        quantity: 1,
+      }),
+      units: [
+        calculator.recalculatePointsForUnit({
+          ...woses,
+          id: randomUuid(),
+          pointsPerUnit: woses.base_points,
+          pointsTotal: woses.base_points * 12,
+          quantity: 12,
+        }),
+      ],
+      meta: {
+        ...emptyWarband.meta,
+        num: 2,
+      },
+    });
+  }
+
+  function addMandatoryWoses(roster: Roster) {
+    if (roster.armyList !== "Paths of the Druadan") {
+      return roster;
+    }
+
+    return calculator.recalculateRoster({
+      ...roster,
+      warbands: [...roster.warbands, wosesWarband()],
+    });
   }
 
   function validateAndAdjustForCompulsoryRules(roster: Roster): Roster {
@@ -168,19 +212,21 @@ export const CreateNewRosterModal = () => {
     const rosterNameValue = fillRosterNameIfEmpty(rosterName.trim());
 
     if (armyList.army) {
-      const newRoster = validateAndAdjustForCompulsoryRules({
-        ...emptyRoster,
-        id: getRosterId(rosterNameValue),
-        group: groupId,
-        name: rosterNameValue,
-        armyList: armyList.title,
-        metadata: {
-          ...emptyRoster.metadata,
-          maxPoints: maxRosterPoints ? Number(maxRosterPoints) : undefined,
-          siegeRoster: enableSiege,
-          siegeRole: enableSiege ? rosterSiegeRole : undefined,
-        },
-      });
+      const newRoster = addMandatoryWoses(
+        validateAndAdjustForCompulsoryRules({
+          ...emptyRoster,
+          id: getRosterId(rosterNameValue),
+          group: groupId,
+          name: rosterNameValue,
+          armyList: armyList.title,
+          metadata: {
+            ...emptyRoster.metadata,
+            maxPoints: maxRosterPoints ? Number(maxRosterPoints) : undefined,
+            siegeRoster: enableSiege,
+            siegeRole: enableSiege ? rosterSiegeRole : undefined,
+          },
+        }),
+      );
 
       createRoster(newRoster);
       navigate(`/roster/${newRoster.id}`, { viewTransition: true });
