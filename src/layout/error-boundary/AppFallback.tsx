c454810e5b0d65c1e67470f4460242d17cbcecb6
mhollink <hollink.mh@gmail.com>
2025-09-24 21:19:17 +0200

    Add additional content to the fallback pages

--- src/layout/error-boundary/AppFallback.tsx
+++ src/layout/error-boundary/AppFallback.tsx
@@ -1,6 +1,10 @@
+import { Divider } from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import { download } from "../../hooks/export/useDownload.ts";
 
 export const AppFallback = () => {
   const error = useRouteError();
@@ -16,6 +20,15 @@ export const AppFallback = () => {
     errorMessage = "Unknown error";
   }
 
+  function getLocallyStoredDate() {
+    const collection = JSON.parse(localStorage.getItem("mlb-collection"));
+    const gamestate = JSON.parse(localStorage.getItem("mlb-gamestate"));
+    const matches = JSON.parse(localStorage.getItem("mlb-matches"));
+    const rosters = JSON.parse(localStorage.getItem("mlb-rosters"));
+    const preferences = JSON.parse(localStorage.getItem("mlb-preferences"));
+    return { collection, gamestate, matches, rosters, preferences };
+  }
+
   return (
     <>
       <Container
@@ -23,21 +36,94 @@ export const AppFallback = () => {
         sx={{
           display: "flex",
           flexDirection: "column",
+          textAlign: "center",
           gap: 2,
           p: 2,
+          my: 4,
         }}
       >
-        <Typography variant="h3">Oh no!</Typography>
-        <Typography
-          variant="caption"
-          component="pre"
-          sx={{
-            px: 5,
-            borderLeft: 1,
+        <Typography variant="h3">A Shadow Has Fallen!</Typography>
+        <Typography variant="body1">
+          “Even the smallest code can bring about the greatest of crashes.
+          <br /> Fear not, traveler, for this is but a passing error in the
+          journey.”
+        </Typography>
+
+        <Divider />
+
+        <Typography>
+          You can download your current state to file and help us recover if
+          something goes missing.
+        </Typography>
+        <Button
+          color="primary"
+          variant="outlined"
+          fullWidth
+          onClick={() => {
+            const backupDate = new Date()
+              .toLocaleDateString("en-UK", {
+                day: "2-digit",
+                month: "2-digit",
+                year: "numeric",
+              })
+              .replace(/ /g, "-");
+            const backup = getLocallyStoredDate();
+            download(
+              JSON.stringify(backup, null, 4),
+              `mesbg_list_builder_backup_${backupDate}.json`,
+              "application/json",
+            );
           }}
         >
-          {errorMessage}
-        </Typography>
+          Click here to download a backup
+        </Button>
+        <Divider />
+
+        <Box
+          sx={{
+            textAlign: "left",
+            display: "flex",
+            flexDirection: "column",
+            gap: 1,
+          }}
+        >
+          <Box sx={{ p: 2 }}>
+            <Typography>
+              <b>Help us help you!</b>
+            </Typography>
+            <Typography>
+              Please do share a screenshot of this page with us either via email
+              or in our discord. We do like to know what happend and resolve the
+              issue.
+            </Typography>
+          </Box>
+
+          <Typography textAlign="center" variant="body2">
+            <a href="https://discord.gg/MZfUgRtV56">
+              https://discord.gg/MZfUgRtV56
+            </a>{" "}
+            or{" "}
+            <a href="mailto:support@mesbg-list-builder.com?subject=MESBG List Builder (v2024) - Bug/Correction">
+              support@mesbg-list-builder.com
+            </a>
+            .
+          </Typography>
+
+          <Typography variant="body1" fontWeight="bold">
+            Error log:
+          </Typography>
+          <Typography
+            variant="caption"
+            component="pre"
+            sx={{
+              px: 5,
+              borderLeft: 1,
+              textAlign: "left",
+            }}
+          >
+            {errorMessage}
+          </Typography>
+        </Box>
       </Container>
     </>
   );
--- src/layout/error-boundary/RootFallback.tsx
+++ src/layout/error-boundary/RootFallback.tsx
@@ -1,3 +1,79 @@
 export const RootFallback = () => {
-  return <>Tests!</>;
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          padding: "1rem",
+        }}
+      >
+        <div
+          style={{
+            textAlign: "center",
+            maxWidth: "66ch",
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+          }}
+        >
+          <h3 style={{ fontSize: "2rem" }}>A Shadow Has Fallen!</h3>
+          <p>
+            “This page was cast into the fires of Mount Doom… and did not
+            return”
+          </p>
+          <div>
+            <div
+              role="alert"
+              style={{
+                background: "rgb(253, 237, 237)",
+                color: "rgb(95, 33, 32)",
+                padding: "1.2rem",
+                backgroundColor: "lightpink",
+                borderRadius: "2px",
+                textAlign: "left",
+                marginBottom: "1rem",
+              }}
+            >
+              <h4>Help us help you!</h4>
+              <div>
+                Please do share a screenshot of this page with us either via
+                email or in our discord. We do like to know what happend and
+                resolve the issue.
+              </div>
+            </div>
+            <p>
+              <a href="https://discord.gg/MZfUgRtV56">
+                https://discord.gg/MZfUgRtV56
+              </a>{" "}
+              or{" "}
+              <a href="mailto:support@mesbg-list-builder.com?subject=MESBG List Builder (v2024) - Bug/Correction">
+                support@mesbg-list-builder.com
+              </a>
+              .
+            </p>
+          </div>
+
+          <a
+            href="https://mesbg-list-builder.com"
+            style={{
+              padding: "0.2rem",
+              textTransform: "uppercase",
+              fontSize: "0.9rem",
+              background: "rgb(229, 246, 253)",
+              borderColor: "rgb(1, 67, 97)",
+              borderStyle: "solid",
+              borderRadius: "2px",
+              borderWidth: "1px",
+              color: "rgb(1, 67, 97)",
+              cursor: "pointer",
+              textDecoration: "none",
+            }}
+          >
+            Return to the Shire
+          </a>
+        </div>
+      </div>
+    </>
+  );
 };
